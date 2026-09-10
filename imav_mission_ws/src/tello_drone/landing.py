3dc0bb01f6c257cd57dff1e90332e6019a9ec4f2
Quique1409 <enriquemedranorabak@hotmail.com>
2026-09-10 15:45:09 -0600

    Se agrega scripts para los modulos del tello

---
+++ imav_mission_ws/src/tello_drone/landing.py
@@ -0,0 +1,12 @@
+"""
+Landing Script for Tello Drone
+
+Camara del Tello frontal 5MP a 30 FPS, sin camara inferior
+Plataforma de desarrollo: entorno virtual Python ....
+Plataforma de aterrizaje: Aruco ID:5, 80x80cm, movimiento horizontal a 0.1m/s, y con un giro 15° en Pitch.
+
+"""
+
+import cv2
+import numpy as np
+import time
---
+++ imav_mission_ws/src/tello_drone/main.py
@@ -0,0 +1,6 @@
+"""
+Pumas - LIRA - UNAM
+Script for Tello Drone Control
+
+Planeación de la misión completa, completar la ruta.
+"""
